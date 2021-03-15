d2a64e575b83d2b6ef8ba86237ceb5d2a3f375d8
Andre Peixoto <peixoto.amp@gmail.com>
2021-03-15 12:40:09 -0300

    app completed

---
+++ README.md
@@ -0,0 +1,4 @@
+# Random User Generator
+
+Simple app built using Vue.js for pulling random user details (external API) and
+rendering it to a 'user card'.
---
+++ app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: 'Andre',
+      lastName: 'Peixoto',
+      email: 'andre@peixotodev.com',
+      gender: 'male',
+      picture:
+        'https://avatars.githubusercontent.com/u/35813862?s=460&u=4a80bf9994c09de556bbb19f6cfad5d77ff1cc76&v=4',
+    };
+  },
+
+  methods: {
+    async getUser() {
+      const response = await fetch('https://randomuser.me/api');
+      const { results } = await response.json();
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
+  },
+});
+
+app.mount('#app');
